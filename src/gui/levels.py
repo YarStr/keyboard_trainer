896e69041f0878ed2a20bc5e96efc12e4b7f1b70
YarStr <staya_03@mail.ru>
2023-05-21 22:24:56 +0500

    Стилизован виджет уровней

--- src/gui/levels.py
+++ src/gui/levels.py
@@ -1,6 +1,8 @@
 from functools import partial
 
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel, \
+    QGridLayout
 
 from src.gui.widget_type import WidgetType
 from src.logic.statistics import Statistics
@@ -8,6 +10,14 @@ from src.gui.ui_model import UIModel
 from src.logic.user_data_model import UserDataModel
 
 from src.storage.levels import Level
+
+
+def _get_level_choose_widget() -> QWidget:
+    message = f'Выбери уровень!'
+    label = QLabel(message)
+    label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+    label.setProperty('class', 'levels-text')
+    return label
 
 
 class LevelWidget(QWidget):
@@ -19,33 +29,49 @@ class LevelWidget(QWidget):
 
     def get_main_layout(self) -> QVBoxLayout:
         layout = QVBoxLayout()
-        layout.addWidget(QLabel(self.get_hello_message()))
-        layout.addWidget(QLabel('Выбери уровень:'))
+        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(self._get_hello_message_widget())
+        layout.addWidget(_get_level_choose_widget())
         self.add_level_widgets_to_layout(layout)
         return layout
 
-    def get_hello_message(self) -> str:
-        return f'Итак, {self._data_model.user_name}, вот твоя статистика!'
+    def _get_hello_message_widget(self) -> QWidget:
+        message = f'Статистика профиля: <b>{self._data_model.user_name}</b>'
+        label = QLabel(message)
+        label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        label.setProperty('class', 'levels-text')
+        return label
 
     def add_level_widgets_to_layout(self, layout: QVBoxLayout):
         for level in Level:
-            layout.addWidget(self.get_level_button(level))
-            for stat in Statistics:
-                layout.addWidget(QLabel(self.get_stat_message(level, stat)))
+            button = self._get_level_button(level)
+            layout.addWidget(button, alignment=Qt.AlignmentFlag.AlignCenter)
 
-    def get_level_button(self, level: Level) -> QPushButton:
+            for stat in Statistics:
+                layout.addWidget(self._get_stat_message_label(level, stat))
+
+    def _get_level_button(self, level: Level) -> QPushButton:
         button = QPushButton(level.value)
+        button.setProperty('class', 'button')
+        button.setFixedSize(400, 100)
         button.clicked.connect(partial(self.start_game, level))
         return button
 
+    def _get_stat_message_label(
+            self, level: Level, stat: Statistics) -> QLabel:
+        label = QLabel(self.get_stat_message(level, stat))
+        label.setProperty('class', 'stat-text')
+        label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        return label
+
     def get_stat_message(self, level: Level, stat: Statistics) -> str:
         if level == Level.BATTLE and stat == Statistics.BEST_TIME:
-            return 'Сразись с монстрами и не допусти ошибок!'
+            return 'Сразись с монстрами!'
         else:
             value = self._data_model.stat_block[level.name][stat.name]
             stat_name = stat.value
             if value is None:
-                value = 'Нет результата'
+                value = 'Нет'
             return f'{stat_name}: {value}'
 
     def start_game(self, level: Level) -> None:
